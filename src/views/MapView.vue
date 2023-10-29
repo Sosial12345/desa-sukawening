522c8130c308efcaa365474c3d75825485cd9da2
Sosial12345 <144506846+Sosial12345@users.noreply.github.com>
2023-10-29 13:25:29 +0700

    Update MapView.vue
    add batas desa

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -101,7 +101,7 @@ export default {
     async loadSomeGeoJson() {
       const nextIndex = this.layerData.length;
 
-      const response = await fetch("/ds_nyule.geojson");
+      const response = await fetch("/batas-desa-sukawening.json");
       const data = await response.json();
 
       console.log(data);
