5a384ae054f7fb894997afe44171fb740d63013b
Sosial12345 <144506846+Sosial12345@users.noreply.github.com>
2023-10-29 09:43:50 +0700

    Update HomeView.vue
    fix visi misi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,7 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
 
     <div class="flex-1 p-10">
-      <p class="text-3xl font-semibold">Visi & Misi Desa</p>
+      <p class="text-3xl font-semibold">Visi & Misi Desa Sukawening</p>
       <p class="text-lg font-semibold">Visi</p>
       <p>
         “Mewujudkan desa sukawening Tangguh ( tanggap,Giat dan utuh )”
@@ -20,25 +20,23 @@ import Sidebar from "../components/Sidebar.vue";
       <p class="text-lg font-semibold">Misi</p>
       <ul>
         <li>
-          1. •	Menyelenggarakan  Pemerintahan Desa yang bersih,terbebas dari korupsi serta bentuk penyelewengan lainnya.
+          1. Menyelenggarakan  Pemerintahan Desa yang bersih,terbebas dari korupsi serta bentuk penyelewengan lainnya.
         </li>
         <li>
-          2. •	Meningkatkan perekonomian masyarakat melalui pendampingan berupa penyuluhan khusus kepada UMKM,wiraswasta dan petani
+          2. Meningkatkan perekonomian masyarakat melalui pendampingan berupa penyuluhan khusus kepada UMKM,wiraswasta dan petani
         </li>
         <li>
-          3. •	Meningkatkan prekonomian masyarakat untuk mencapai taraf kehidupan yang lebih baik dan layak sehingga menjadi desa yang maju dan mandiri
+          3. Meningkatkan prekonomian masyarakat untuk mencapai taraf kehidupan yang lebih baik dan layak sehingga menjadi desa yang maju dan mandiri
         </li>
         <li>
-          4. •	Menumbuh kembangkan perekonomian masyarakat melalui pemberdayaan UMKM dan mengembangkan ketahanan pangan menjadi sentra ekonomi masyarakat.
+          4. Menumbuh kembangkan perekonomian masyarakat melalui pemberdayaan UMKM dan mengembangkan ketahanan pangan menjadi sentra ekonomi masyarakat.
         </li>
         <li>
-          5. •	Meningkatkan Kembali sarana olah raga,bola volley,bulutangkis dan lainnya
+          5. Meningkatkan Kembali sarana olah raga,bola volley,bulutangkis dan lainnya
         </li>
         <li>
-          6. •	Penggalian potensi untuk program desa wisata guna meningkatkan perekonomian masyarakat
+          6. Penggalian potensi untuk program desa wisata guna meningkatkan perekonomian masyarakat
         </li>
-        
-          
       </ul>
     </div>
   </main>
